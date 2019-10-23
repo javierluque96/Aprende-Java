283bb3da7e5eeebfe85f25a55c0c233e4007cd3d
javier.luque96 <javier.lr96@gmail.com>
2019-10-23 18:01:29 +0200

    Adding new exercises

---
+++ Ejercicios Tema 05/Ejercicicios_Extra/ExtraBucles6.java
@@ -0,0 +1,45 @@
+public class ExtraBucles6 {
+  
+  public static void main (String[] args) {
+    System.out.print("Introduce la altura (al menos 5): ");
+    int altura = Integer.parseInt(System.console().readLine());
+    
+    int anchura = altura;
+    int espacios = 0;
+    int i, j;
+    int huecos;
+    
+    for (i = 0; i < altura; i++) {
+      huecos = 0;
+      if (espacios == 3) {
+	espacios = 0;
+      }
+      // Introduce espacios
+      for (j = 0; j < espacios; j++) {
+	System.out.print("-");
+	huecos++;
+      }
+      
+      // Introduce asteriscos
+      for (j = 0; j < altura / 2; j++){
+	if (huecos < altura) {
+	  System.out.print("o");
+	}
+	huecos++;
+	
+	if (huecos < altura) {
+	  System.out.print("-");
+	}
+	
+	huecos++;
+	if (huecos < altura) {
+	  System.out.print("-");
+	}
+	huecos ++;
+      }
+      
+      espacios++;
+      System.out.println();
+    }
+  } 
+}
