4c2f9b3afce70f49c734bfae7b0fcdc24257cc9d
javier.luque96 <javier.lr96@gmail.com>
2019-11-11 00:19:30 +0100

    añadiendo ejercicios

---
+++ Ejercicios_Tema07/7.2_Arrays_bidimensionales/T7Ejercicio1.java
@@ -0,0 +1,42 @@
+public class T7Ejercicio1 {
+  
+  
+  public static void main (String[] args) {
+    int[][] numero = new int[3][6]; // 3 Filas y 6 Columnas 
+    int fila, columna;
+    
+    // Fila 0
+    numero[0][0] = 0;
+    numero[0][1] = 30;
+    numero[0][2] = 2;
+    numero[0][5] = 5;
+    
+    // Fila 1
+    numero[1][0] = 75;
+    numero[1][4] = 0;
+    
+    // Fila 2
+    numero[2][2] = -2;
+    numero[2][3] = 9;
+    numero[2][5] = 11;
+    
+
+    System.out.print("|Array num |");
+    for (columna = 0; columna < 6; columna++) {
+      System.out.printf("Columna%2d|", columna);
+    }
+    
+    System.out.println();
+    
+    for (fila = 0; fila < 3; fila++) {
+      System.out.printf("|  Fila%2d  |", fila);
+      
+      for (columna = 0; columna < 6; columna++) {
+	System.out.printf(" %5d   |", numero[fila][columna]);
+      }
+      
+      System.out.println();
+    }
+  } 
+}
+
---
+++ Ejercicios_Tema07/7.2_Arrays_bidimensionales/T7Ejercicio2.java
@@ -0,0 +1,62 @@
+public class T7Ejercicio2 {
+  
+  
+  public static void main (String[] args) {
+    int[][] num = new int[4][5]; 
+    int[] sumaFila = new int[4];
+    int[] sumaColumna = new int[5];
+    int fila, columna;
+    int total = 0;
+    
+    // Recoge los 20 números en el array 
+    System.out.println("Introduce 20 números enteros: ");
+    for (fila = 0; fila < 4; fila++) {
+      System.out.println("Fila " + fila);
+      
+      for (columna = 0; columna < 5; columna++) {
+	num[fila][columna] = Integer.parseInt(System.console().readLine());
+      }
+    }
+    
+    // Sumatorio de las filas, las columnas y el total
+    for (fila = 0; fila < 4; fila++) {
+      for (columna = 0; columna < 5; columna++) {
+	sumaFila[fila] += num[fila][columna];
+	sumaColumna[columna] += num[fila][columna];
+	total += num[fila][columna];
+      }
+    }
+    
+    // Imprime el array
+    System.out.print("\n|Array num |");
+    
+    for (columna = 0; columna < 5; columna++) {
+      System.out.printf("Columna%2d|", columna);
+    }
+    
+    System.out.print("Sumatorio|");
+    
+    for (fila = 0; fila < 4; fila++) {
+      System.out.print("\n|");
+      System.out.printf("  Fila%2d  |", fila);
+      
+      for (columna = 0; columna < 6; columna++) {
+	if (columna == 5) { // Imprime sumatorio de la fila
+	  System.out.printf(" %5d   |", sumaFila[fila]);
+	} else {
+	  System.out.printf(" %5d   |", num[fila][columna]);
+	}
+      }
+    }
+    
+    // Imprime la fila con los sumatorios de columnas y el total
+    System.out.print("\n|Sumatorio |");
+    
+    for (columna = 0; columna < 5; columna++) {
+      System.out.printf("%6d   |", sumaColumna[columna]);
+    }
+    
+    System.out.printf(" %5d   |", total);
+  } 
+}
+
